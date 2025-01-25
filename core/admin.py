154869ee57ef8ae6d68196e905498e7bb37191e2
Yash <yash17agrawal@gmail.com>
2025-01-25 07:54:05 +0530

    feat: add import export for categories

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
 from core.models.models import Category, Customer, Order, OrderItem, Product, Shipment, Vendor
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+        exclude = ('created_at', 'modified_at',)
+
+
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
 
 
 # Register your models here.
 admin.site.register(Vendor)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
